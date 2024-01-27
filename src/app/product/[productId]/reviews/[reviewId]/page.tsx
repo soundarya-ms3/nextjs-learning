818584d5495679ccb1b5f260e5c53e894627ec60
soundarya-ms3 <soundaryams03@gmail.com>
2024-01-27 10:37:18 +0530

    Not found Page

--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,12 @@
+import { notFound } from "next/navigation";
 export default function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       review {params.reviewId} of product {params.productId}{" "}
