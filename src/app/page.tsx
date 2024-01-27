4e4549b888425a4a35a144e97dc8621a60d4d277
soundarya-ms3 <soundaryams03@gmail.com>
2024-01-27 15:56:41 +0530

    Link Component Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home(){
-    return <h1>Home page</h1>
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Products</Link>
+    </>
+  );
 }
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 export default function Product() {
   return (
     <>
+      <Link href="/">Home</Link>
       <h2>Product list</h2>
-      <h2>Product list 1</h2>
-      <h2>Product list 2</h2>
+      <h2>
+        {" "}
+        <Link href="product/1">Product list 1</Link>
+      </h2>
+      <h2>
+        <Link href="product/2">Product list 2</Link>
+      </h2>
     </>
   );
 }
